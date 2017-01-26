213f47f4e933a0f8e02ab147c91d1d7eb4ca92c7
huvanile <jeremy.huval@gmail.com>
2017-01-26 10:36:20 -0600

    Add project files.

---
+++ timeEntryBot/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("timeEntryBot")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Grant Thornton LLP")>
+<Assembly: AssemblyProduct("timeEntryBot")>
+<Assembly: AssemblyCopyright("Copyright © Grant Thornton LLP 2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("faecceb3-272c-46bd-b6b1-c3c4b82202ed")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
