d99ce2877f01dfb0ea195bb0f5210abc7c50bcda
huvanile <jeremy.huval@gmail.com>
2017-01-27 07:47:45 -0600

    messed with IE automation a tiny bit more

--- timeEntryBot/Models/Browser.vb
+++ timeEntryBot/Models/Browser.vb
@@ -1,41 +1,56 @@
-﻿Imports System.Windows.Forms
+﻿Imports mshtml
+Imports System.Text
+Imports System.Windows.Forms
 
 Public Class Browser
-    Private ie As Object
+    Private _ie As Object
+    Private _html As String
+    Private _allElements As mshtml.IHTMLElementCollection
 
     Sub New()
-        ie = New Object
-        ie = CreateObject("InternetExplorer.Application")
-        ie.Visible = True '''''''
+        _ie = New Object
+        _ie = CreateObject("InternetExplorer.Application")
+        _ie.Visible = True '''''''
     End Sub
 
 #Region "Answer Questions"
     Public Function IsCodeVisible()
 
     End Function
+
 #End Region
 
 #Region "Actions"
     Public Sub LoadCMS()
-        ie.Navigate("https://cms.gt.com")
+        _ie.Navigate("https://cms.gt.com")
         waitForPageLoad()
     End Sub
 
     Private Sub waitForPageLoad()
-        Do While ie.Busy Or ie.ReadyState <> 4
+        Do While _ie.Busy Or _ie.ReadyState <> 4
             Threading.Thread.Sleep(500)
         Loop
     End Sub
 
     Public Sub LoadTimePage()
-        Dim tdCollection As Object = ie.Document.GetElementsByTagName("td")
+        Dim tdCollection As Object = _ie.Document.GetElementsByTagName("td")
         For Each element As Object In tdCollection
             If element.GetAttribute("title") = "Time" Then
                 element.Children(0).click
                 Exit For
             End If
         Next
+
         waitForPageLoad()
+
+        Dim wc As New Net.WebClient
+        Dim bHTML() As Byte = wc.DownloadData(_ie.address) 'bytes of html (not usable yet)
+        _html = New UTF8Encoding().GetString(bHTML)
+        Dim doc As mshtml.IHTMLDocument2 = New mshtml.HTMLDocument
+        doc.clear()
+        doc.write(_html)
+        _allElements = doc.all
+        doc.close()
     End Sub
 #End Region
 
